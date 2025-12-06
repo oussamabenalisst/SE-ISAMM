b842293fcaaa919be541c535f1afd1214065d71a
Oussama Benali <118560924+oussamabenalisst@users.noreply.github.com>
2025-12-06 18:25:50 +0100

    add ex1 tp4

---
+++ tr1/tp4 ex1.sh
@@ -0,0 +1,19 @@
+fich1 cmde1
+chmod 755 cmde1
+chmod o-r cmde1
+chmod cmde1 
+--------------------
+fich2 cmde2
+chmod 755 cmde2
+chmod 521 cmde2
+chmod cmd2
+-------------------
+fich3 cmde3
+chmod 755 cmde3
+chmod u-rx,g-r,o-x cmde3
+chmod 214 cmde3
+------------------
+fich4 cmde14
+chmod 755 cmde14
+chmod 150 cmde14
+chmod u-rw,o-rx cmde14
